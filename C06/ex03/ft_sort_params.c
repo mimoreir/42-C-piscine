4566492dee1d4f30589d33088e4c6bacea40d4e3
Miguel Moreira Montenegro Da Silva <mimoreir@c1r4s4.42porto.com>
2022-07-28 17:30:49 +0100

    C06 final

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -27,16 +27,61 @@ void	ft_putstr(char *str)
 	ft_putchar('\n');
 }
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 || *s2)
+	{
+		if (*s1 == *s2)
+			;
+		else
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while(j < argc)
+		{
+			if(j >= argc)
+				break;
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
 
-	i = argc - 1;
-	while (i > 0)
+	i = 1;
+	if(argc > 1)
 	{
-		ft_putstr(argv[i]);
-		i--;
+		ft_sort_params(argc, argv);
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			i++;
+		}
 	}
+	return (0);
 }
 
 
